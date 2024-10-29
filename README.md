543f24ed1d23255ac62cfdc5a9e3fa732fd05494
xuqing <1094997271@qq.com>
2024-10-29 09:05:29 +0800

    init repository

---
+++ README.md
@@ -0,0 +1,12 @@
+# generate-vue-component README
+一键生成vue组件，组件目录结构：
+- my-component
+    - my-component.vue
+    - my-component.scss
+    -  index.js
+
+
+### 组件引用
+import MyComponent from '@/components/my-component'
+
+
---
+++ src/extension.ts
@@ -0,0 +1,64 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.generate-vue-component', async (uri: vscode.Uri) => {
+        const componentName = await vscode.window.showInputBox({ prompt: '输入组件名称' });
+        
+        if (componentName && uri?.fsPath) {
+            const folderPath = path.join(uri.fsPath, componentName);
+            createComponentFolder(folderPath, componentName);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function createComponentFolder(folderPath: string, componentName: string) {
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+        createComponentFiles(folderPath, componentName);
+        vscode.window.showInformationMessage(`组件 ${componentName} 创建成功！`);
+    } else {
+        vscode.window.showErrorMessage(`组件 ${componentName} 已存在！`);
+    }
+}
+
+function createComponentVue(folderPath: string, componentName: string) {
+	const componentContent = `<template>
+    <div>${componentName} 组件</div>
+</template>
+
+<script>
+export default {
+    name: '${componentName}',
+	data() {
+		return {};
+	},
+	mounted() {},
+	beforeDestroy() {},
+	methods: {}
+}
+</script>
+
+<style lang="scss" src="./${componentName}.scss" scoped></style>
+`;
+  fs.writeFileSync(path.join(folderPath, `${componentName}.vue`), componentContent);
+}
+
+function createCompontCSS(folderPath: string, componentName: string) {
+	const componentContent = `@import '@/styles/element-variables.scss';`;
+	fs.writeFileSync(path.join(folderPath, `${componentName}.scss`), componentContent);
+}
+
+function createComponentFiles(folderPath: string, componentName: string) {
+    
+	createComponentVue(folderPath, componentName);
+	createCompontCSS(folderPath, componentName);
+    const indexContent = `import comp from './${componentName}.vue';
+    export default comp;`;
+    fs.writeFileSync(path.join(folderPath, 'index.js'), indexContent);
+}
+
+export function deactivate() {}
